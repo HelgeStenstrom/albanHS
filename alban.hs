d24aff71d3ffb2bdcc2ed0b13ef6c3b08d0cbc1c
Helge Stenström <h.stenstrom@gmail.com>
2015-10-16 08:15:18 +0200

    Laddar upp
    Jag vet inte riktigt hur det här funkar, så jag laddar upp lite…

--- alban.hs
+++ alban.hs
@@ -9,6 +9,12 @@
 
 import Data.Bits
 
+-- Imports of QuickCheck are in conflict with imports of Data.Bits.
+-- Vad kan jag göra åt det?
+-- Specifikt: .&. finns i båda.
+
+-- import Test.QuickCheck
+-- import Test.QuickCheck.All
 
 -- Registertyper
 -- Alban SPI-register är 40 bitar.
@@ -47,7 +53,7 @@ import Data.Bits
 data Field = Field {value :: Int,
                    lsbPos :: Int,
                    width :: Int}
-                   deriving (Show)
+                   deriving (Show, Eq)
 -- Denna definition har nackdelen att värdet måste definieras. Jag kanske bara vill definiera lsbPos och width.
 
 -- Exempelvis när man ska extrahera fält från ett registerord, givet
@@ -60,7 +66,7 @@ valField (Field v _ _ ) = v
 lsbField (Field _ pos _) = pos
 widthField (Field _ _ w) = w
 
-
+type RegContents = [Field]
 
 -- Det känns problematiskt att man ändrar värden för registret
 -- samtidigt som man kan påverka dess adress. Adressen är en del av
@@ -68,11 +74,31 @@ widthField (Field _ _ w) = w
 -- Man kan inte kopiera från ett register till ett annat.
 -- 
 data Reg = Reg {
-          fields :: [Field],
+          fields :: RegContents,
           address :: Address
-          } deriving (Show)
--- xReg is VCO_F at address 1
-vcofReg = Reg [xfa, xfb] 1
+          } deriving (Show, Eq)
+
+vcofRegExample = Reg [xfa, xfb] 1
+vcofReg fs = Reg fs 1
+-- type VcofReg fs = Reg fs 1
+
+-- Ordningen i första argumentet spelar roll:
+-- vcofRegExample == vcofReg [xfa, xfb]
+-- vcofRegExample /= vcofReg [xfb, xfa]
+
+
+data CrioReg a = VcoF Reg a
+               | VcoI Reg a
+                 deriving (Show)
+
+data CrioReg2 a  = MiscReg a
+              | VcoFReg a
+              | VcoIReg a
+                deriving (Show)
+
+crioReg w16
+        | addressCrioWord w16 == 1 = VcoFReg 1
+
 
 -- The value a field contributes with i a register word, based on its LSB position and its own value.
 inPosition :: Field -> Int
@@ -83,7 +109,7 @@ inPosition f = shiftL ((valField f) .&. (shiftL 1 (widthField f) - 1))  (lsbFiel
 valuesFieldList fs = map valField fs
 xValuesFL = valuesFieldList[xfa, xfb]
 
-wordFL :: [Field] -> Int
+wordFL :: RegContents -> Int
 wordFL r = foldl (.|.) 0 (map inPosition r)
 
 wordReg :: Reg -> Int
@@ -120,16 +146,101 @@ nameOfCrioWord w16
     | otherwise = "not yet defined"
                                  
 
-f1OfCrioWord w16 
+f1ofCrioWord w16 
     | addressCrioWord w16 == 1 = Just "VCO_F"
     | otherwise = Nothing
 
-f2OfCrioWord w16 
+f2ofCrioWord w16 
     | addressCrioWord w16 == 1 = Just (Reg [xfa, xfb] 1)
     | otherwise = Nothing
 
-f3OfCrioWord w16 
+f3ofCrioWord w16 
     | addressCrioWord w16 == 1 = Reg [xfa, xfb] 1
     | otherwise = Reg [xfb, xfa] 17
 
 
+data SomeField  = FaField Field  
+                | FbField Field
+                  deriving (Show)
+
+f4ofCrioWord w16 
+    | addressCrioWord w16 == 1 = Reg [Field 8 5 5, Field 7 11 5] 1
+
+f5ofCrioWord w16 
+    | addressCrioWord w16 == 1 = Reg [Field valFb 5 5, Field valFa 11 5] 1
+    where 
+      valFa = (shiftR w16 11) .&. 31
+      valFb = (shiftR w16 5) .&. 31
+
+-- Denna funktion lämnar ifrån sig register Reg av en dålig typ, för
+-- den är ganska anonym. De olika registren skiljer sig åt enbart
+-- genom sitt datainnehåll. Inte genom sin typ.
+-- Dessutom blir det onödigt mycket att skriva. 
+f6ofCrioWord w16 
+    | addressCrioWord w16 == 1 = Reg [Field valFb posB 5, Field valFa posA 5] 1
+    | addressCrioWord w16 == 2 = Reg [Field valCoreSel posCoreSel 2] 2
+    where 
+      valFa = (shiftR w16 posA) .&. 31
+      valFb = (shiftR w16 posB) .&. 31
+      posA = 11
+      posB = 5
+      valCoreSel = (shiftR w16 posCoreSel) .&. 3
+      posCoreSel = 10
+
+
+-- Jag vill ha olika register som är olika typer, men ändå inte. De
+-- har genemsamma nämnare.
+-- Varför vill jag ha olika typer? För att jag tror att det ökar säkerheten. 
+
+type Blå = Int
+type Röd = Int
+
+f7 1 = 1::Blå
+f7 2 = 2::Röd
+
+main = putStrLn "Hej, världen!"
+huvud = putStrLn "Detta är funktionen 'huvud'."
+
+
+
+
+
+-- Idéer från #haskell, användare Hijiri:
+-- Don't store the length and position with the data.
+-- Have a type for each register type.
+-- and then for your interpreting function output a sum type that includes all of them as alternatives
+-- like data PossibleRegisters = R1 RegisterType1 | R2 RegisterType2 | ...
+-- and interpret :: Word16 -> PossibleRegisters
+-- or something like that
+-- and then with each constructor you know what register type it has, and you can choose to do something different based on that
+-- Hijiri: So my interpret function would take the 4 LSB, and then
+-- return different RegisterType1, RegisterType2 etc, depending on
+-- them. And these RegisterTypeN values would take their data
+-- depending on the 12 MSB.
+-- 22:44:16 Hijiri: HelgeS: yep
+-- Hijiri: though (pedantically) the interpret function will be returning the big sum type that has those RegisterN types as alternatives
+-- Hijiri: it won't be returning those types directly
+-- HelgeS: Hijiri: Now, my registers consist of fields of various lengths and LSB positions, and the same names, lengths and LSB positions could be used for the functions that construct a 16-bit register value from a register. And I don't to define these lengths and LSB positions in more than one place.
+-- do you mean you don't want to define those numbers in more than one place?
+--  for that you could just define them at the top level
+--  HelgeS: My idea is to have "data Reg a = FieldA Reg a | FieldB a deriving (Show)"
+--  HelgeS: Hijiri: My idea is that for example one register, called
+--  RegA has two fields, called FieldA and FieldB, that both share the
+--  properties that fields have, such as a length in bits, an LSB
+--  position within the register word, and (possibly) a value.
+--  Hijiri: HelgeS: Is Reg a supposed to be a register?
+--  HelgeS: Yes
+--  Hijiri: it wouldn't make sense to define it as FieldA (Reg a) | FieldB then, since that would mean it either has FieldA or FieldB, not both
+--  Hijiri: what I would do is first define a Field type
+--  Hijiri: actually wait
+--  HelgeS: I'm sorry, I think I have to take a pause here and read my code carefully again.
+--  HelgeS: I don't remember all my intentions with the code.
+--  Hijiri: why do you need to store the length and LSB position in the data?
+--   Hijiri: it seems like that should be handled by the interpret and encoding functions
+--   HelgeS: Hijiri: Perhaps I don't have to do that. But I want to do
+--   conversions in both directions, between arbitarary 16-bit words
+--   and well-typed registers with sub-fields. All 16 registers
+--   together comprise most of the state of the modelled circuit.
+--  Hijiri: HelgeS: the functions in both directions can each use the length and position data
+--  Hijiri: you don't need to store the length and position data because you already know them from what kind of register it is
+--  HelgeS: Good points!
